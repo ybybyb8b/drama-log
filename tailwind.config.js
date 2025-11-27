c1db448385ddeb509f7c707802fe86eb0e84bc2b
苦力热巴 <eva596091951@gmail.com>
2025-11-27 11:20:18 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // 你可以在这里扩展自定义主题，目前保持默认即可
+    },
+  },
+  plugins: [],
+}
